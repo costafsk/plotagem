f1342b7dc5beffc931fce7594e9c6ac281ffa114
Diego Costa <36934264+costafsk@users.noreply.github.com>
2018-07-06 17:38:56 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const columns = rows[i].split(',');
             for (let j=0; j<columns.length; j++) {
                 columns[j] = columns[j].replace('"', '');
+                columns[j] = columns[j].replace('"', '');
             }
             // Armazenamento
             const date = {
@@ -27,8 +28,48 @@ document.addEventListener('DOMContentLoaded', function() {
             };
             dates.push(date);
         }
-        console.table(dates);
+        options(dates);
     }
+    
+    const select = document.querySelector('#city');
+    
+    function options(dates) {
+        for (let i=0; i<dates.length; i++) {
+            const newOption = document.createElement('option');
+            newOption.textContent = dates[i].municipio;
+            newOption.value = newOption.textContent;
+            select.appendChild(newOption);  
+        }
+    }
+    
+    const ul = document.querySelector('.mdl-list');
+    const button = document.querySelector('.mdl-button');
+    
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const li = document.createElement('li');
+        li.className = 'mdl-list__item';
+        const spanName = document.createElement('span');
+        spanName.className = 'mdl-list__item-primary-content';
+        spanName.textContent = select.value;
+        const spanInput = document.createElement('span');
+        spanInput.className = 'mdl-list__item-secondary-action';
+        const labelInput = document.createElement('label');
+        labelInput.classList.add('mdl-checkbox', 'mdl-js-checkbox', 'mdl-js-ripple-effect', 'mdl-js-ripple-effect--ignore-events');
+        labelInput.htmlFor = spanName.textContent;
+        const input = document.createElement('input');
+        input.className = 'mdl-checkbox__input';
+        input.setAttribute('type', 'checkbox');
+        input.id = spanName.textContent;
+
+        
+        labelInput.appendChild(input);
+        spanInput.appendChild(labelInput);
+        li.appendChild(spanName);
+        li.appendChild(spanInput);
+        ul.appendChild(li);
+    });
+
 });
 
 
